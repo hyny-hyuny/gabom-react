db5a3643b56641434c939799e8e2491270a9457c
juhyuny <juhyun5358@gmail.com>
2025-02-25 14:30:09 +0900

    [Chore] 절대경로구성 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
-import "./styles/main.css";
+import App from '@/app';
+import "@/styles/main.css";
 
 const root = document.getElementById('root');
 
