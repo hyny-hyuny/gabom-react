f66edad87bc94d88ddcd3971efb0526357d65e6b
soohyn <sooheyonjo@gmail.com>
2025-02-26 00:08:20 +0900

    [BugFix] vite 설정 파일에 base 추가
    - base 경로 '/' 설정
    
    Resolves 파비콘 안 뜨고 에러 #5

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'node:url';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [
+  base: '/',
+  plugins: [
     tailwindcss(),
-		react({
-			jsxRuntime: 'automatic',
-		}),
-	],
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
   server: {
     host: 'localhost',
     port: 3000,
