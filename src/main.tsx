0b54c95caa5f61338c975210d94a60efab087701
soohyn <sooheyonjo@gmail.com>
2025-02-26 10:40:23 +0900

    [BugFix] main.tsx 롤백 후 index.html 경로 재설정
    Resolves 파비콘 안 뜨고 에러 #5

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import App from '@/app';
+import "@/styles/main.css";
 
 const root = document.getElementById('root');
 
 if (root) {
 	createRoot(root).render(
-		<h1>
-			test main.tsx
-		</h1>
+		<StrictMode>
+			<App />
+		</StrictMode>
 	);
 }
