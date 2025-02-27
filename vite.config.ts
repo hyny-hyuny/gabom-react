4f7beb16e930cdbc26a445ac5d44172e764edbcc
soohyn <sooheyonjo@gmail.com>
2025-02-27 14:33:11 +0900

    [Chore] svg components 사용 설정
    - svg를 컴포넌트로 사용하기 위한 설정
    - vite 설정 라이브러리 설치
    - tsconfig에 타입 파일 추가
    
    Resolves 내비게이션 바 개발 #8

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   base: '/',
   plugins: [
     tailwindcss(),
+    svgr(),
     react({
       jsxRuntime: 'automatic',
     }),
