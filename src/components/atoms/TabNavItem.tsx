6025283241a5042dc77c95e867362fb75f0db8ba
soohyn <sooheyonjo@gmail.com>
2025-02-27 23:31:43 +0900

    [Feat] svg 컴포넌트 아이콘 적용
    - 머지 이후 svg 설정 적용 후 Icon 데이터입력
    
    Resolves 탭 내비게이션 개발 #15

--- src/components/atoms/TabNavItem.tsx
+++ src/components/atoms/TabNavItem.tsx
@@ -8,7 +8,7 @@ type TabNavItemProps = TabNavItemData & {
 function TabNavItem({
   searchParam,
   label,
-  // Icon,
+  Icon,
   currentSearchParam,
   handleSearchParam,
 }: TabNavItemProps) {
@@ -20,9 +20,9 @@ function TabNavItem({
 
   return (
     <li className='flex flex-1'>
-      <button className={`${isCurrent ?'text-tertairy' :'border-transparent'} border-b-2  w-full flex justify-center items-center py-1 px-4`} onClick={handleClickNavLink}>
-        {/* <Icon /> */}
-        <span className={`text-sm/relaxed font-medium`}>{label}</span>
+      <button className={`${isCurrent ?'text-tertiary' :'border-transparent'} border-b-2  w-full flex gap-1 justify-center items-center py-1 px-4`} onClick={handleClickNavLink}>
+        <Icon width={18} />
+        <span className={`text-xs/relaxed font-medium`}>{label}</span>
       </button>
     </li>
   );
--- src/components/molecules/TabNav.tsx
+++ src/components/molecules/TabNav.tsx
@@ -1,9 +1,15 @@
-// import TimeIcon from '../../assets/time.svg?react';
-// import SaveIcon from '../../assets/save.svg?react';
+import TimeIcon from '../../assets/time.svg?react';
+import SaveIcon from '../../assets/save.svg?react';
+import PlaceIcon from '../../assets/place.svg?react';
+import ReviewIcon from '../../assets/review.svg?react';
 import TabNavItem from '../atoms/TabNavItem';
 import { useSearchParams } from 'react-router';
 
-export type TabNavItemSearchParam = 'review' | 'place' | 'bookmark' | 'reservation';
+export type TabNavItemSearchParam =
+  | 'review'
+  | 'place'
+  | 'bookmark'
+  | 'reservation';
 
 export type IconType = React.FC<React.SVGProps<SVGSVGElement>> | string;
 
@@ -14,19 +20,19 @@ export interface TabNavItemData {
 }
 
 const tabNavItems: TabNavItemData[] = [
-  { searchParam: 'review', label: '리뷰', Icon: '' },
-  { searchParam: 'place', label: '방문장소', Icon: '' },
-  { searchParam: 'bookmark', label: '저장', Icon: '' },
-  { searchParam: 'reservation', label: '예약', Icon: '' },
+  { searchParam: 'review', label: '리뷰', Icon: ReviewIcon },
+  { searchParam: 'place', label: '방문장소', Icon: PlaceIcon },
+  { searchParam: 'bookmark', label: '저장', Icon: SaveIcon },
+  { searchParam: 'reservation', label: '예약', Icon: TimeIcon },
 ];
 
 function TabNav() {
-  const [searchParams,setSearchParams] = useSearchParams();
-  const currentSearchParam = searchParams.get('tab') as TabNavItemSearchParam
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentSearchParam = searchParams.get('tab') as TabNavItemSearchParam;
 
   const handleSearchParam = (searchParam: TabNavItemSearchParam) => {
     searchParams.set('tab', searchParam);
-    setSearchParams(searchParams)
+    setSearchParams(searchParams);
   };
 
   const tabNavItemsMap = tabNavItems.map((item) => {
@@ -41,8 +47,11 @@ function TabNav() {
   });
 
   return (
-    <nav className='inner-border px-6 border-gray-100' aria-label="내 정보 목록 내비게이션">
-      <ul className='flex h-11'>{tabNavItemsMap}</ul>
+    <nav
+      className="top-inner-border-bottom px-6 border-gray-100"
+      aria-label="내 정보 목록 내비게이션"
+    >
+      <ul className="flex h-11">{tabNavItemsMap}</ul>
     </nav>
   );
 }
