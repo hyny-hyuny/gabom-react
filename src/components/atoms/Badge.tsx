d4672c7d660b1acb70aaae91fbbf5bc93af81990
juhyuny <juhyun5358@gmail.com>
2025-03-06 14:20:43 +0900

    [Modify] font size 변경

--- src/components/atoms/Badge.tsx
+++ src/components/atoms/Badge.tsx
@@ -8,7 +8,7 @@ function Badge({ children }: BadgeProps) {
   return (
     <p
       className={tm(
-        'label-sm px-custom-2 py-0.5 rounded-3xs inline absolute top-custom-1 left-custom-1',
+        'label-xs px-custom-2 py-0.5 rounded-3xs inline absolute top-custom-1 left-custom-1',
         children === '전시중'
           ? 'bg-green'
           : children === '전시 종료'
