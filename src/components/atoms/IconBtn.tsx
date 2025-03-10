bf8c1b81b61228232e332f0db1aa1c034404afad
soohyn <sooheyonjo@gmail.com>
2025-03-10 23:49:30 +0900

    [Feat] 아이콘 버튼 생성
    - 간단한 버튼 구현
    - 카운터 +,- 버튼에 사용 예정
    
    Resolves 카운터 개발 #49

---
+++ src/components/atoms/IconBtn.tsx
@@ -0,0 +1,26 @@
+import { tm } from '@/utils/tw-merge';
+import { ComponentProps } from 'react';
+
+type IconBtnProps = ComponentProps<'button'> & {};
+
+function IconBtn({
+  children,
+  type = 'button',
+  className,
+  ...buttonProps
+}: IconBtnProps) {
+  return (
+    <button
+      type={type}
+      className={tm(
+        'p-1 rounded-2xs bg-primary disabled:bg-gray-100  text-contents-content-primary disabled:text-gray-400',
+        className
+      )}
+      {...buttonProps}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default IconBtn;
