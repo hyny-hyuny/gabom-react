bbbb0757804230683dc19658217c63be531adaff
soohyn <sooheyonjo@gmail.com>
2025-03-10 23:50:18 +0900

    [Feat] 카운터 개발
    - 외부에서 상태 관리 변수 전달
    - 최대, 최소값 설정
    
    Resolves 카운터 개발 #49

--- src/components/pages/ReviewPage.tsx
+++ src/components/pages/ReviewPage.tsx
@@ -2,14 +2,21 @@ import { useState } from 'react';
 import SearchBar from '../atoms/SearchBar';
 import ReviewCard from '../molecules/ReviewCard';
 import Input from '../atoms/Input';
+import Counter from '../molecules/Counter';
 
 function ReviewPage() {
-  const [searchText,setSearchText] = useState<string>('')
+  const [searchText, setSearchText] = useState<string>('');
+  const [count, setCount] = useState(0);
 
   return (
     <section>
-      <SearchBar placeholder="검색어를 입력하세요" searchText={searchText} setSearchText={setSearchText}/>
-      <Input label='hi' />
+      <SearchBar
+        placeholder="검색어를 입력하세요"
+        searchText={searchText}
+        setSearchText={setSearchText}
+      />
+      <Input label="hi" />
+      <Counter count={count} setCount={setCount} minCount={2} maxCount={10} />
       <ReviewCard
         title="< 소행성이 빛을 내는 방법>전시를 다녀왔습니다. 두 줄 이상이 되면 ellipse 되나요????"
         content="진짜 맨날 근처오면 와서 4-5개씩은 사가는 곳입니다. 비건디저트 특유의 향이 안나서 더 좋아요! 세 줄 이상이 들어가면 "
