73223156a450029ebd309d096659ed13c70b8fbf
juhyuny <juhyun5358@gmail.com>
2025-03-20 15:54:24 +0900

    [Feat] 세션에 따른 페이지 접근 제한 기능

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Navigate, Route, Routes, Session } from 'react-router';
 import MyPage from './components/pages/MyPage';
 import ReviewPage from './components/pages/ReviewPage';
 import PlacePage from './components/pages/PlacePage';
@@ -7,24 +7,46 @@ import Layout from './layout/Layout';
 import EditProfilePage from './components/pages/EditProfilePage';
 import WriteReviewPage from './components/pages/WriteReviewPage';
 import LoginPage from './components/pages/LoginPage';
+import { useEffect, useState } from 'react';
+import supabase from './lib/supabase';
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    void supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session as unknown as Session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session as unknown as Session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="review">
-            <Route index element={<ReviewPage />} />
-            <Route path="write" element={<WriteReviewPage />} />
+        {session ? (
+          <Route path="/" element={<Layout />}>
+            <Route path="review">
+              <Route index element={<ReviewPage />} />
+              <Route path="write" element={<WriteReviewPage />} />
+            </Route>
+            <Route path="place" element={<PlacePage />} />
+            <Route path="exhibition" element={<ExhibitionPage />} />
+            <Route path="my">
+              <Route index element={<MyPage />} />
+              <Route path="edit" element={<EditProfilePage />} />
+            </Route>
           </Route>
-          <Route path="place" element={<PlacePage />} />
-          <Route path="exhibition" element={<ExhibitionPage />} />
-          <Route path="my">
-            <Route index element={<MyPage />} />
-            <Route path="edit" element={<EditProfilePage />} />
-          </Route>
-        </Route>
+        ) : (
+          <Route path="*" element={<Navigate to="/login" />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
