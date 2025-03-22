74818eaee54371178c635d76d7db1321dd9f55e5
soohyn <sooheyonjo@gmail.com>
2025-03-22 20:23:57 +0900

    [Feat] 내 리뷰 목록 필터 생성
    - 최신순 토글, 날짜 선택 필터로 구성된 필터 컴포넌트 생성
    
    Resolves 마이페이지 개발 #53

--- src/components/organisms/ReviewList.tsx
+++ src/components/organisms/ReviewList.tsx
@@ -1,6 +1,7 @@
 import supabase from '@/lib/supabase';
 import { useEffect, useState } from 'react';
 import ReviewCard from '../molecules/ReviewCard';
+import ListFilter from '../molecules/ListFilter';
 
 interface Review {
   id: string;
@@ -12,13 +13,15 @@ interface Review {
 
 function ReviewList() {
   const [reviews, setReviews] = useState<Review[]>([]);
+  const [isLatest, setIsLatest] = useState(true);
+  const [monthFilter, setMonthFilter] = useState<string>(new Date().toString());
 
   const getReviews = async () => {
     try {
       const result = await supabase.from('review').select('*');
 
       if (result.error) {
-        throw new Error(); 
+        throw new Error();
       }
 
       setReviews(result.data);
@@ -32,24 +35,33 @@ function ReviewList() {
   }, []);
 
   return (
-    <ul className='flex flex-col gap-3'>
-      {reviews.map((item) => {
-        return (
-          <ReviewCard
-            key={item.id}
-            content={item.content}
-            title={item.title}
-            starRate={'5'}
-            profileImg={''}
-            reviewImg={''}
-            writer={item.user_id}
-            date={'25년 6월 22일'}
-            linkTo={`/${item.id}`}
-            counts={[0, 0]}
-          />
-        );
-      })}
-    </ul>
+    <section>
+      <h2 className="sr-only">리뷰 목록</h2>
+      <ListFilter
+        monthFilter={monthFilter}
+        latest={isLatest}
+        setMonthFilter={setMonthFilter}
+        setLatest={setIsLatest}
+      />
+      <ul className="flex flex-col gap-3">
+        {reviews.map((item) => {
+          return (
+            <ReviewCard
+              key={item.id}
+              content={item.content}
+              title={item.title}
+              starRate={'5'}
+              profileImg={''}
+              reviewImg={''}
+              writer={item.user_id}
+              date={'25년 6월 22일'}
+              linkTo={`/${item.id}`}
+              counts={[0, 0]}
+            />
+          );
+        })}
+      </ul>
+    </section>
   );
 }
 
